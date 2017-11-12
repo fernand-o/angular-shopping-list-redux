c46fc4f4da7ac864dfe3962d35e1c3fdc42857ab
Fernando Almeida <fernandoprsbr@gmail.com>
2017-11-12 17:11:10 -0200

    Adds missing routes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { Ingredient } from '../../shared/ingredient.model';
 import { RecipeService } from '../recipe.service';
@@ -8,12 +9,21 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params) => {
+        this.recipe = this.recipeService.getRecipe(+params['id']);
+      }
+    )
+  }
 
   toShoppingList(ingredients: Ingredient[]) {
     this.recipeService.addIngredientsToShoppingList(ingredients);
   }
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,13 +8,13 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Panquecas', 'Ótimas para o café da manhã', 'http://culinaria.culturamix.com/blog/wp-content/gallery/receitas-de-massa-de-panqueca-1-1/Receitas-de-Massa-de-Panqueca-2.jpg',
+    new Recipe(1, 'Panquecas', 'Ótimas para o café da manhã', 'http://culinaria.culturamix.com/blog/wp-content/gallery/receitas-de-massa-de-panqueca-1-1/Receitas-de-Massa-de-Panqueca-2.jpg',
       [
         new Ingredient('Ovos', 2),
         new Ingredient('Farinha', 1),
         new Ingredient('Agua', 5)
       ]),
-    new Recipe('Hamburguer', 'O melhor hambúrguer do Brasil', 'https://static.vix.com/pt/sites/default/files/styles/large/public/bdm/receita/receita-de-carne-de-hamburguer.jpg',
+    new Recipe(2, 'Hamburguer', 'O melhor hambúrguer do Brasil', 'https://static.vix.com/pt/sites/default/files/styles/large/public/bdm/receita/receita-de-carne-de-hamburguer.jpg',
       [
         new Ingredient('Pão', 1),
         new Ingredient('Meat', 3),
@@ -26,6 +26,15 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(id: number) {
+    return this.recipes.find(
+      (value: Recipe) => {
+        if (value.id === id)
+          return true;
+      }
+    )
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
