e076ac520c4556775d37bd13ae0ac4808b4b638e
Fernando Almeida <fernandoprsbr@gmail.com>
2017-11-19 19:54:19 -0200

    Loads recipes using lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,16 @@ import { HttpModule } from '@angular/http';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuardService } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +27,6 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     SharedModule,
     AppRoutingModule,
     ShoppingListModule,
-    RecipesModule,
     AuthModule
   ],
   providers: [
